0d504b17acde7129ba20a5efd8650b207cd41ef0
Алексей Розеган <rozeganalexey@gmail.com>
2025-04-07 14:20:08 +0200

    Zainicjuj pusty projekt Django o nazwie teatr_55666

---
+++ teatr_55666/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for teatr_55666 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teatr_55666.settings')
+
+application = get_asgi_application()
