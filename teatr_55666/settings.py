663b52d597edbdf8a76308ac691961d31d0ee054
Алексей Розеган <rozeganalexey@gmail.com>
2025-04-27 19:53:23 +0200

    Pobieranie DRF do projektu

--- teatr_55666/settings.py
+++ teatr_55666/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'teatr_service'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
