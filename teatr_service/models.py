23e52ff0222d8bcb0de1c5fca9ea3b3bb4b863f2
Алексей Розеган <rozeganalexey@gmail.com>
2025-04-27 19:56:42 +0200

    Twordzenie modele dla models.py

--- teatr_service/models.py
+++ teatr_service/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Gatunek(models.Model):
+    nazwa = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nazwa
+
+
+class Aktor(models.Model):
+    imie = models.CharField(max_length=255)
+    nazwisko = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.imie} {self.nazwisko}"
+
+
+class Sztuka(models.Model):
+    tytul = models.CharField(max_length=255)
+    opis = models.TextField()
+    gatunki = models.ManyToManyField(Gatunek, related_name='sztuki', blank=True)
+    aktorzy = models.ManyToManyField(Aktor, related_name='sztuki', blank=True)
+
+    def __str__(self):
+        return self.tytul
+
+
+class SalaTeatralna(models.Model):
+    nazwa = models.CharField(max_length=255)
+    liczba_rzedow = models.PositiveIntegerField()
+    miejsca_w_rzedzie = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.nazwa
+
+
+class Przedstawienie(models.Model):
+    sztuka = models.ForeignKey(Sztuka, on_delete=models.CASCADE, related_name='przedstawienia')
+    sala_teatralna = models.ForeignKey(SalaTeatralna, on_delete=models.CASCADE, related_name='przedstawienia')
+    czas_wystepu = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.sztuka.tytul} o {self.czas_wystepu}"
+
+
+class Rezerwacja(models.Model):
+    data_utworzenia = models.DateTimeField(auto_now_add=True)
+    uzytkownik = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="rezerwacje"
+    )
+
+    def __str__(self):
+        return f"Rezerwacja #{self.id} przez {self.uzytkownik}"
+
+
+class Bilet(models.Model):
+    rzad = models.PositiveIntegerField()
+    miejsce = models.PositiveIntegerField()
+    przedstawienie = models.ForeignKey(Przedstawienie, on_delete=models.CASCADE, related_name='bilety')
+    rezerwacja = models.ForeignKey(Rezerwacja, on_delete=models.CASCADE, related_name='bilety')
+
+    class Meta:
+        unique_together = ('przedstawienie', 'rzad', 'miejsce')
