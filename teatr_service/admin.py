d742190ba82b8360b07cfd4ca94b47ff09273ab8
Алексей Розеган <rozeganalexey@gmail.com>
2025-04-29 16:21:09 +0200

    Dostosowanie admin.py za pomocą filtrów

--- teatr_service/admin.py
+++ teatr_service/admin.py
@@ -9,12 +9,52 @@ from .models import (
     Bilet
 )
 
-admin.site.register(Gatunek)
-admin.site.register(Aktor)
-admin.site.register(Sztuka)
-admin.site.register(SalaTeatralna)
-admin.site.register(Przedstawienie)
-admin.site.register(Rezerwacja)
-admin.site.register(Bilet)
+
+@admin.register(Gatunek)
+class GatunekAdmin(admin.ModelAdmin):
+    list_display = ("id", "nazwa")
+    search_fields = ("nazwa",)
 
 
+@admin.register(Aktor)
+class AktorAdmin(admin.ModelAdmin):
+    list_display = ("id", "imie", "nazwisko")
+    search_fields = ("imie", "nazwisko")
+
+
+@admin.register(Sztuka)
+class SztukaAdmin(admin.ModelAdmin):
+    list_display = ("id", "tytul")
+    search_fields = ("tytul", "opis")
+    filter_horizontal = ("gatunki", "aktorzy")
+
+
+@admin.register(SalaTeatralna)
+class SalaTeatralnaAdmin(admin.ModelAdmin):
+    list_display = ("id", "nazwa", "liczba_rzedow", "miejsca_w_rzedzie")
+    search_fields = ("nazwa",)
+    list_filter = ("liczba_rzedow",)
+
+
+@admin.register(Przedstawienie)
+class PrzedstawienieAdmin(admin.ModelAdmin):
+    list_display = ("id", "sztuka", "sala_teatralna", "czas_wystepu")
+    list_filter = ("sala_teatralna", "czas_wystepu")
+    search_fields = ("sztuka__tytul",)
+    autocomplete_fields = ("sztuka", "sala_teatralna")
+
+
+@admin.register(Rezerwacja)
+class RezerwacjaAdmin(admin.ModelAdmin):
+    list_display = ("id", "uzytkownik", "data_utworzenia")
+    list_filter = ("data_utworzenia",)
+    search_fields = ("uzytkownik__username",)
+    date_hierarchy = "data_utworzenia"
+
+
+@admin.register(Bilet)
+class BiletAdmin(admin.ModelAdmin):
+    list_display = ("id", "przedstawienie", "rezerwacja", "rzad", "miejsce")
+    list_filter = ("przedstawienie", "rzad")
+    search_fields = ("rezerwacja__uzytkownik__username",)
+    autocomplete_fields = ("przedstawienie", "rezerwacja")
