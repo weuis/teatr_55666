53af320ace017f5985e0116ff7ded2982ab7b43a
Алексей Розеган <rozeganalexey@gmail.com>
2025-05-16 13:54:37 +0200

    Dodanie mozliwosci logowania do aplikacji

--- teatr_55666/settings.py
+++ teatr_55666/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'frontend',
     'payments',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from users.views import RegisterUserView
+from users.views import RegisterUserView, LoginUserView
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
+    path("login/", LoginUserView.as_view(), name="login"),
 ]
 
 app_name = 'users'
